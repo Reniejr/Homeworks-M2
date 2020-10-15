0c2e61ff12709f99b2495e5bf68e561fb1376127
Reniejr <reniejr@gmail.com>
2020-10-15 14:39:33 +0200

    Basic Structure

---
+++ M2-D4/index.js
@@ -0,0 +1,103 @@
+/* -------------------------------------------------------------------------------------------------------- */
+
+/*window.onload = function (){
+
+    //INTRO - LOGO ANIMATION
+
+    //LAYOUT HIDDEN under for the INTRO
+    let header = document.getElementById('header')
+    let main = document.getElementById('main')
+    let footer = document.getElementById('footer')
+    header.style.display = 'none' 
+    main.style.display = 'none'
+    footer.style.display = 'none'
+    
+
+    //INTRO VIDEO
+    let intro = document.createElement('video')
+    intro.id = 'intro'
+    intro.src = 'Logo RJD Developer.mp4'
+    intro.type = 'video/mp4'
+    intro.style.width = '100%'
+    intro.style.height = '100%'
+    intro.style.position = 'absolute'
+
+    //INTRO - PROPERTY
+    intro.muted = 'true'
+    intro.play()
+    intro.preload= 'none'
+
+    document.body.appendChild(intro)
+
+    //Fade out at the end
+    intro.addEventListener('ended', function(){
+
+        intro.style.opacity = '1'
+        intro.style.transition = 'opacity 3s ease'
+
+        intro.style.opacity = '0'
+        intro.style.transition = 'opacity 3s ease'
+
+        intro.style.display = 'none'
+
+
+        //Fade in of layout
+
+            
+        header.style.display = 'block' 
+        main.style.display = 'block'
+        footer.style.display = 'block'
+
+        
+        header.style.opacity = '0' 
+        main.style.opacity = '0'
+        footer.style.opacity = '0'
+
+        header.style.transition = 'opacity 2s ease'
+        main.style.transition = 'opacity 2s ease'
+        footer.style.transition = 'opacity 2s ease'
+
+        header.style.opacity = '1' 
+        main.style.opacity = '1'
+        footer.style.opacity = '1'
+
+        header.style.transition = 'opacity 2s ease'
+        main.style.transition = 'opacity 2s ease'
+        footer.style.transition = 'opacity 2s ease'
+
+    },false)
+}*/
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------------------------------------- */
