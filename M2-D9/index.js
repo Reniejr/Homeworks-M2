cdcac650e7c045d8ca1c5c8d25a4d9c6f2b83857
Reniejr <reniejr@gmail.com>
2020-10-22 16:50:13 +0200

    Ex until n10

--- M2-D9/index.js
+++ M2-D9/index.js
@@ -1,4 +1,4 @@
-window.onload = function (){
+/*window.onload = function (){
 
     //INTRO - LOGO ANIMATION
 
@@ -6,12 +6,14 @@ window.onload = function (){
     document.body.style.backgroundColor = 'white'
     let mainContainer = document.querySelector('#main_container')
     mainContainer.style.opacity = '0'
-    mainContainer.style.display = 'none'
+    //mainContainer.style.display = 'none'
+
+    //mainContainer.style.display = 'block'
 
     //INTRO VIDEO
     let intro = document.createElement('video')
     intro.id = 'intro'
-    intro.src = 'Logo RJD Developer.mp4'
+    intro.src = 'Logo Animato RJD Developer.mp4'
     intro.type = 'video/mp4'
     intro.style.width = '100%'
     intro.style.position = 'absolute'
@@ -32,7 +34,7 @@ window.onload = function (){
     intro.addEventListener('ended', function(){
 
         /*intro.style.opacity = '1'
-        intro.style.transition = 'opacity 3s ease'*/
+        intro.style.transition = 'opacity 3s ease'
 
         intro.style.opacity = '0'
         intro.style.transition = 'opacity 3s ease'
@@ -42,7 +44,6 @@ window.onload = function (){
         document.body.style.width = '100%'
         document.body.style.height = '100%'
 
-        mainContainer.style.display = 'block'
 
         //fade in elements after INTRO
 
@@ -51,10 +52,100 @@ window.onload = function (){
 
 
         mainContainer.style.opacity = '1'
-        mainContainer.style.transition = 'opacity 1s ease'
+        mainContainer.style.transition = 'opacity 3s ease'
 
         document.body.removeChild(containerIntro)
 
     },false)
  
+}*/
+
+let breadcrumb = document.querySelector('#breadcrumb')
+let bSlide = document.querySelector('#breadcrumb div')
+
+bSlide.addEventListener('click', function(){
+
+    if(breadcrumb.style.marginTop = '-5vh'){
+        breadcrumb.style.marginTop = '0'
+    }
+    else if(breadcrumb.style.marginTop = '0'){
+        breadcrumb.style.marginTop = '-5vh'
+    }
+
+})
+
+let removebtn = document.querySelectorAll('#album_details .btn')
+let tracksRow = document.querySelectorAll('#album_details .row')
+let tracksTable = document.getElementById('album_details')
+
+for( let a = 0; a < removebtn.length; a++){
+
+    removebtn[a].addEventListener('click', function(){
+
+        tracksRow[a+1].style.opacity = '0'
+        tracksRow[a+1].style.transition = 'opacity 3s ease'
+        setTimeout(function(){tracksTable.removeChild(tracksRow[a+1])}, 3000)
+    })
+}
+
+let albumSource = document.querySelector('.modal-body input:nth-of-type(1)')
+let addSource = document.querySelector('.modal-body input:nth-of-type(2)')
+let albumTitle = document.querySelector('.modal-body input:nth-of-type(3)')
+let addTitle = document.querySelector('.modal-body input:nth-of-type(4)')
+let titlesArray = []
+let sourceArray = []
+
+let source = function(){
+
+    let newAlbum = document.createElement('div')
+    newAlbum.className= 'col'
+    let albumImg = document.createElement('img')
+    albumImg.src = albumSource.value
+    newAlbum.appendChild(albumImg)
+    sourceArray.push(newAlbum)
+}
+
+
+let titleAdd = function(){
+
+    let newTitle = document.createElement('div')
+    newTitle.className = 'col'
+    let titleWord = document.createElement('h6')
+    titleWord.innerText = addTitle.value
+    newTitle.appendChild(titleWord)
+    titlesArray.push(newTitle)
+}
+
+let addTrack = function(){
+
+    let newLenght = document.createElement('div')
+    newLenght.className = 'col'
+    let lenghtWord = document.createElement('h6')
+    lenghtWord.innerText = '?'
+    newLenght.appendChild(lenghtWord)
+
+    let newRemove = document.createElement('div')
+    newRemove.className = 'col'
+    let removeButton = document.createElement('button')
+    removeButton.classList.add = 'btn'
+    removeButton.classList.add = 'btn-outline-secondary'
+    removeButton.value = 'REMOVE'
+    removeButton.addEventListener('click', function(){
+
+        tracksRow[a+1].style.opacity = '0'
+        tracksRow[a+1].style.transition = 'opacity 3s ease'
+        setTimeout(function(){tracksTable.removeChild(tracksRow[a+1])}, 3000)
+    } )
+    newRemove.appendChild(removeButton)
+
+    let newRow = document.createElement('div')
+    newRow.className= 'row'
+
+    newRow.appendChild(sourceArray[0])
+    newRow.appendChild(titlesArray[0])
+    newRow.appendChild(newLenght)
+    newRow.appendChild(newRemove)
+
+    tracksTable.appendChild(newRow)
+
 }
